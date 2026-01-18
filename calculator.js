70ef7e49d98f6d0d791b2bf17783458b622d6043
niloarthur-lgtm <niloarthurpr@gmail.com>
2026-01-18 14:16:02 -0300

    add files

---
+++ calculator.js
@@ -0,0 +1,8 @@
+function sum(){
+    let num1 = document.getElementById('n1').value;
+    let num2 = document.getElementById('n2').value;
+
+    let total = Number(num1) + Number(num2);
+
+    document.getElementById('res').innerText = "the result is:" + total;
+}
