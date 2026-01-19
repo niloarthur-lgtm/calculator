edd81c1e04dd6f5421cfc7e5322e76fb63955398
niloarthur-lgtm <niloarthurpr@gmail.com>
2026-01-19 18:46:30 -0300

    add minus (code fix)
    subtraction (no html)

--- calculator.js
+++ calculator.js
@@ -1,24 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport">
-        <link rel="stylesheet" href="calculator.css">
-        <script src="calculator.js" defer></script>
-        <title>calculator</title>
-    </head>
-    <body>
-        <div class="title">
-            <h1>calculator</h1>
-        </div>
-        <div class="calculator">
-        <input type="number" id="n1" placeholder="number1">
-        <input type="number" id="n2" placeholder="number2">
-        <br>
-        <button onclick="sum()">sum</button>
-        <button onclick="less()">less</button>
-        <hr>
-        <p id="res"></p> 
-        </div>
-    </body>
-</html>
+function sum(){
+    let num1 = document.getElementById('n1').value;
+    let num2 = document.getElementById('n2').value;
+
+    let total = Number(num1) + Number(num2);
+
+    document.getElementById('res').innerText = "the result is:" + total;
+}
+
+function less(){
+    let num1 = document.getElementById('n1').value;
+    let num2 = document.getElementById('n2').value;
+
+    let total = Number(num1) - Number(num2);
+
+    document.getElementById('res').innerText = "the result is:" + total;
+}
