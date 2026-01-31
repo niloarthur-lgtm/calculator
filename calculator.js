a7bdb862265f04d68dbd87d826dda40b51c323ce
Nilo Arthur <niloarthurpr@gmail.com>
2026-01-31 18:57:25 -0300

    add * and /
    add multiplication and division

--- calculator.js
+++ calculator.js
@@ -1,17 +1,34 @@
-function sum(){
+function sum() {
     let num1 = document.getElementById('n1').value;
     let num2 = document.getElementById('n2').value;
 
-    let total = Number(num1) + Number(num2);
+    let all = Number(num1) + Number(num2);
 
-    document.getElementById('res').innerText = "the result is:" + total;
+    document.getElementById('res').innerText = "= " + all;
 }
 
-function less(){
+function less() {
     let num1 = document.getElementById('n1').value;
     let num2 = document.getElementById('n2').value;
 
-    let total = Number(num1) - Number(num2);
+    let all = Number(num1) - Number(num2);
 
-    document.getElementById('res').innerText = "the result is:" + total;
+    document.getElementById('res').innerText = "= " + all;
+}
+
+function mult() {
+    let num1 = document.getElementById('n1').value;
+    let num2 = document.getElementById('n2').value;
+
+    let all = Number(num1) * Number(num2);
+
+    document.getElementById('res').innerText = "= " + all;
+}
+function divi(){
+ let num1 = document.getElementById('n1').value;
+ let num2 = document.getElementById('n2').value;
+
+ let all = Number(num1) / Number(num2);
+
+ document.getElementById('res').innerText = "= " + all;
 }
