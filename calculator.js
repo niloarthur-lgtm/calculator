d6a5b94f3fc1ff9d2b91b615cea450398b523fb6
Nilo Arthur <niloarthurpr@gmail.com>
2026-02-01 12:01:43 -0300

    update to rememer the numbers

--- calculator.js
+++ calculator.js
@@ -1,34 +1,41 @@
+let total = 0;
+
 function sum() {
+
     let num1 = document.getElementById('n1').value;
-    let num2 = document.getElementById('n2').value;
 
-    let all = Number(num1) + Number(num2);
+    total = Number(total) + Number(num1);
 
-    document.getElementById('res').innerText = "= " + all;
+
+    document.getElementById('res').innerText = "total= " + total + " + ";
 }
 
 function less() {
     let num1 = document.getElementById('n1').value;
-    let num2 = document.getElementById('n2').value;
 
-    let all = Number(num1) - Number(num2);
+    total = Number(total) - Number(num1);
 
-    document.getElementById('res').innerText = "= " + all;
+    document.getElementById('res').innerText = "total= " + total + " - ";
 }
 
 function mult() {
     let num1 = document.getElementById('n1').value;
-    let num2 = document.getElementById('n2').value;
 
-    let all = Number(num1) * Number(num2);
+    total = Number(total) * Number(num1);
 
-    document.getElementById('res').innerText = "= " + all;
+    document.getElementById('res').innerText = "total= " + total + " * ";
 }
-function divi(){
- let num1 = document.getElementById('n1').value;
- let num2 = document.getElementById('n2').value;
 
- let all = Number(num1) / Number(num2);
+function divi() {
+    let num1 = document.getElementById('n1').value;
 
- document.getElementById('res').innerText = "= " + all;
+    total = Number(total) / Number(num1);
+
+    document.getElementById('res').innerText = "total= " + total + " / ";
+}
+// invisible text: (alt + 0160)
+
+function reset() {
+    total = 0;
+    document.getElementById('res').innerText = "total= " + total;
 }
